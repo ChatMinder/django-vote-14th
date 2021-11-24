9ddd264976bdc23d063a76216ea5d0f9aed4e3c1
junhwan <ujunhwan@gmail.com>
2021-11-24 16:56:28 +0900

    [feat] URL 패턴 수정

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from rest_framework import urlpatterns
 
-from api.views import HelloView
+from api.views import UserView 
 
 urlpatterns = [
-    path('hello', HelloView.as_view()),
+    path('users', UserView.as_view()),
 ]
