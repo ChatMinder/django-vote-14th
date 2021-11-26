396477267e860239e23e156b945e68084d813a2d
chaeri93 <chaeri93@ewhain.net>
2021-11-26 19:06:23 +0900

    QuestionViewSet 생성

--- polls/views.py
+++ polls/views.py
@@ -6,4 +6,4 @@ from polls.serializers import QuestionSerializer
 
 class QuestionViewSet(ModelViewSet):
     serializer_class = QuestionSerializer
-    queryset = Question
+    queryset = Question.objects.all()
