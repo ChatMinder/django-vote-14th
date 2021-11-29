adb4cd7596417b643ac6bf5c719fc951df4e03f4
junhwan <ujunhwan@gmail.com>
2021-11-29 15:00:50 +0900

    [feat] 본인 조회 permission 추가

--- api/permissions.py
+++ api/permissions.py
@@ -8,3 +8,10 @@ class IsSuperuser(BasePermission):
         
     def has_object_permission(self, request, view, obj):
         return request.user.is_authenticated and request.user.is_superuser
+
+class IsOwnerOrSuperuser(BasePermission):
+    def has_permission(self, request, view):
+        return True
+    
+    def has_object_permission(self, request, view, obj):
+        return request.user.is_authenticated and (obj == request.user or request.user.is_superuser)
