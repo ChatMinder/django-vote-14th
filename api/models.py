2f5aaa0f6e9c8ece9b9701cc407f8c6c8425cd17
chaeri93 <chaeri93@ewhain.net>
2021-12-01 03:22:18 +0900

    [chore] user model에 voted 추가

--- api/models.py
+++ api/models.py
@@ -41,7 +41,7 @@ class User(AbstractBaseUser, PermissionsMixin, BaseModel):
     is_superuser = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
-
+    voted = models.BooleanField(default=False)
 
     objects = UserManager()
 
