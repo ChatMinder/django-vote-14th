3d360b81e3127c1ee6540d9069b2e27c37954da0
junhwan <ujunhwan@gmail.com>
2021-12-01 10:46:50 +0900

    [feat] 중복체크 url 추가

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from rest_framework import urlpatterns
-from api.views import AuthView, AuthViewHttpOnly, UserDetailView, UserView 
+from api.views import AuthView, AuthViewHttpOnly, UserDetailView, UserDuplicateView, UserView 
 
 urlpatterns = [
     path('users', UserView.as_view()),
     path('users/<int:pk>', UserDetailView.as_view()),
+    path('users/duplicate', UserDuplicateView.as_view()),
     path('auth/token', AuthView.as_view()),
     path('auth/token/only', AuthViewHttpOnly.as_view())
 ]
